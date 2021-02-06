26a4dc84a4fbd9cc1385aca549f1c54058c52c31
LAPTOP-SRF2UGMI\Asus <miezlanrezza1219@gmail.com>
2021-02-06 12:25:22 +0800

    update printf

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 int main(){
-<<<<<<< HEAD
-	printf("Hello World");
-	return 0;
-=======
 	
 	
 	printf("Hello World");
@@ -12,5 +8,4 @@ int main(){
 	return 0;
 	
 	
->>>>>>> 267e5cd74045376657f8a9cba1be2fa06c4a5ab6
 }
